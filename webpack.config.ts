277dbd5a713a6a1e1b223f4de9c745e1030e6fcf
manaptk <tk.manap@gmail.com>
2025-03-03 22:52:05 +0500

    1.1 Add react, react-dom, scss, cssLoader

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,4 +1,3 @@
-import * as webpack from 'webpack';
 import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
 import {BuildEnv, BuildPaths} from "./config/build/types/config";
 import path from "path";
