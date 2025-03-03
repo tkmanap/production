8955307af71d40dbfb5614d38555607540a5c536
manaptk <tk.manap@gmail.com>
2025-03-03 22:51:45 +0500

    1.1 Add react, react-dom, scss, cssLoader

--- config/build/buildLoader.ts
+++ config/build/buildLoader.ts
@@ -7,7 +7,19 @@ export function buildLoader(): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     }
 
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    }
     return [
         typescriptLoader,
+        cssLoader,
     ]
 }
--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -3,9 +3,10 @@ import webpack from "webpack";
 import {buildResolvers} from "./buildResolvers";
 import {buildLoader} from "./buildLoader";
 import {buildPlugins} from "./buildPlugins";
+import {buildDevServer} from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const {paths, mode} = options
+    const {paths, mode, isDev} = options
     return {
         mode,
         entry: paths.entry,
@@ -18,6 +19,8 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
         module: {
             rules: buildLoader(),
         },
-        plugins: buildPlugins(options)
+        plugins: buildPlugins(options),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options): undefined,
     };
 }
