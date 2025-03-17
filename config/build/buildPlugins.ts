171012705b0bd27052c9b603e65b9e57d5ee3061
manaptk <tk.manap@gmail.com>
2025-03-17 15:20:00 +0500

    1.4.9 add Bundle Analyzer Plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,7 +1,9 @@
 import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
+import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer'
+
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
     return [
@@ -15,6 +17,9 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
+        }),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false
         })
     ];
 }
