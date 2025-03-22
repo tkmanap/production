2d288920e9ed5e355e44d1dc78eecc6983ae6c60
manaptk <tk.manap@gmail.com>
2025-03-22 20:21:58 +0500

    1.7.1 pipeline github-actions

--- src/shared/ui/Button/Button.test.tsx
+++
@@ -1,9 +0,0 @@
-import {Button} from "shared/ui/Button/Button";
-import {render, screen} from "@testing-library/react";
-
-describe('classNames', () => {
-    test('with only first param', () => {
-        render(<Button>test</Button>)
-        expect(screen.getByText('test')).toBeInTheDocument()
-    })
-});
